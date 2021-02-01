90d49d3e00fe83565690d2f11c7c5c7519a2c7f1
Felipe Monteiro <felipehmsilveira@gmail.com>
2021-02-01 01:38:14 -0300

    Location

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,12 +3,14 @@ import Background from '../components/Background'
 import { Text } from 'react-native-paper'
 import Contacts from './Contacts'
 import SystemInfo from './SystemInfo'
+import Location from './Location'
 
 const Home = ({ navigation }) => {
   return (
     <Background>
         <Contacts/>
         <SystemInfo/>
+        <Location/>
     </Background>
   )
 }
---
+++ src/screens/Location.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { Platform, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+import { Text } from 'react-native-paper'
+
+export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+
+      console.log(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View>
+      <Text>Location Module</Text>
+    </View>
+  );
+}
